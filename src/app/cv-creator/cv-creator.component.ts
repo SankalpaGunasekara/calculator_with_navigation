0a36b74142e61c22082a2effc4c10ab75d1fa7b2
SankalpaGunasekara <sankalpa.gunasekara99@gmail.com>
2023-09-14 16:58:18 +0530

    vehicle detail doc creator functionalities added

--- src/app/cv-creator/cv-creator.component.ts
+++ src/app/cv-creator/cv-creator.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-cv-creator',
@@ -7,21 +8,63 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./cv-creator.component.css']
 })
 export class CvCreatorComponent {
-
-  capitalizedWord: string = ''
   make: string = 'Toyota'
   model: string = 'Hilux'
   grade: string = 'G'
   year: string = "2018"
-  // description:string = ''
+  vehiNumber: string = '123123'
+  engNumber: string = "123123"
+  description:string = ''
 
-  capitalize(value: string, placeholder: string): void {
+  applyFont01:boolean=false
 
-    let cache = ''
-    cache = value.charAt(0).toUpperCase() + value.slice(1, value.length);
-    // placeholder = 
+
+  dataCap(data: string) {
+
+    const cache = data.charAt(0).toUpperCase() + data.slice(1);
+    console.log(cache)
+    return cache;
 
   }
+
+  propertyValues: { [key: string]: string } = {
+
+    make:this.make,
+    model:this.model,
+    grade:this.grade,
+    year: this.year,
+
+  }
+
+  // inputClick(event: Event, variable: string) {
+  //   if (variable in this.propertyValues) {
+  //     this.propertyValues[variable] = '';
+  //   }
+  // }
+  
+
+  // getDataVar(value:any):string{
+  //   return this.dataVariables[value]
+  // }
+
+
+  // capitalize(value: string, placeholder: string):void {
+
+  //   if (placeholder in this.dataVariables) {
+  //     const cache = value.charAt(0).toUpperCase() + value.slice(1);
+  //     this.dataVariables[placeholder] = cache
+  //     console.log(cache)
+  //     console.log(this.dataVariables[placeholder])
+
+
+  //   }
+
+
+  // placeholder = 
+
+  // }
+
+
 
 
 }
